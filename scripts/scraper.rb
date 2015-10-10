5834333676f8d93c7eeb4d9aa2960b1a6943d745
Alex Espinoza <aespinoza@aspria.net>
2015-10-10 12:16:32 -0400

    Update scraper to take in json file name and output sample ids to text file of same name:

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -2,17 +2,26 @@ require 'json'
 require 'pry'
 
 class Scraper
-  def initialize(json_file)
-    @json_file = File.read(json_file)
+  def initialize(json_file_name)
+    @json_file = File.read(File.expand_path("../data/json/#{json_file_name}.json", File.dirname(__FILE__)))
+    @text_file = File.new(File.expand_path("../data/ids/#{json_file_name}.txt", File.dirname(__FILE__)), 'w')
     @samples_hash = JSON.parse(@json_file)
   end
 
   def write_ids_to_file
     @samples_hash.each do |sample|
-      File.open('ids.txt', 'a') { |file| file.puts(sample['_id']) }
+      File.open(@text_file, 'a') { |file| file.puts(sample['_id']) }
     end
+
+    done_scraping
+  end
+
+  def done_scraping
+    puts "Done scraping, file saved to data/ids/#{File.basename(@text_file)}"
   end
 end
 
-scraper = Scraper.new('all_samples.json')
+puts 'Input json file to scrape ids from (do not include .json extension):'
+json_file_name = gets.chomp
+scraper = Scraper.new(json_file_name)
 scraper.write_ids_to_file
