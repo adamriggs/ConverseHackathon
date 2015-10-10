521e5e6ae04a61fd7e573e9c336705e0da625b85
Alex Espinoza <aespinoza@aspria.net>
2015-10-10 15:33:45 -0400

    Update scraper to handle getting ids of loop or one shot samples

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -4,31 +4,43 @@ require 'uri'
 require 'pry'
 
 class Scraper
-  def initialize(instrument_name)
-    @instrument = formatted_for_api(instrument_name)
-    @instrument_file_name = formatted_for_filename(instrument_name)
-    @api_endpoint = "http://hackathon.indabamusic.com/samples?instruments=#{@instrument}&per_page=99999"
-    @text_file = File.new(File.expand_path("../data/ids/#{@instrument_file_name}.json", File.dirname(__FILE__)), 'w')
+  def initialize(instrument, sample_type='all')
+    @instrument = formatted_for_api(instrument)
+    @instrument_filename = formatted_for_filename(instrument)
+
+    set_endpoint_and_filename_for_sample_type(sample_type)
   end
 
-  def formatted_for_api(instrument_name)
-    instrument_name_array = instrument_name.downcase.split(' ')
+  def formatted_for_api(instrument)
+    instrument_string_array = instrument.downcase.split(' ')
 
-    if (instrument_name_array.length > 1)
-      instrument_name_array = instrument_name_array.map {|word| word.capitalize}.join(' ')
-      URI.escape(instrument_name_array)
+    if (instrument_string_array.length > 1)
+      instrument_string_array = instrument_string_array.map {|word| word.capitalize}.join(' ')
+      URI.escape(instrument_string_array)
     else
-      instrument_name_array.first.capitalize
+      instrument_string_array.first.capitalize
     end
   end
 
-  def formatted_for_filename(instrument_name)
-    instrument_name_array = instrument_name.downcase.split(' ')
+  def formatted_for_filename(instrument)
+    instrument_string_array = instrument.downcase.split(' ')
 
-    if (instrument_name_array.length > 1)
-      instrument_name_array.join('_')
+    if (instrument_string_array.length > 1)
+      instrument_string_array.join('_')
     else
-      instrument_name_array.first
+      instrument_string_array.first
+    end
+  end
+
+  def set_endpoint_and_filename_for_sample_type(sample_type)
+    valid_sample_types = ['one_shot', 'loop']
+
+    if sample_type && valid_sample_types.include?(sample_type)
+      @json_file = File.new(File.expand_path("../data/ids/#{@instrument_filename}_#{sample_type}.json", File.dirname(__FILE__)), 'w')
+      @api_endpoint = "http://hackathon.indabamusic.com/samples?instruments=#{@instrument}&per_page=99999&type=#{sample_type}"
+    else
+      @json_file = File.new(File.expand_path("../data/ids/#{@instrument_filename}_all.json", File.dirname(__FILE__)), 'w')
+      @api_endpoint = "http://hackathon.indabamusic.com/samples?instruments=#{@instrument}&per_page=99999"
     end
   end
 
@@ -44,23 +56,22 @@ class Scraper
   end
 
   def write_ids_to_file(instrument_json)
-    File.open(@text_file, 'a') { |file| file.puts("[") }
+    File.open(@json_file, 'a') { |file| file.puts("[") }
 
     instrument_json.each do |sample|
-      File.open(@text_file, 'a') { |file| file.puts("\"#{sample['_id']}\",") }
+      File.open(@json_file, 'a') { |file| file.puts("\"#{sample['_id']}\",") }
     end
 
-    File.open(@text_file, 'a') { |file| file.puts("]") }
+    File.open(@json_file, 'a') { |file| file.puts("]") }
 
-    done_scraping
-  end
-
-  def done_scraping
-    puts "Done scraping, file saved to data/ids/#{File.basename(@text_file)}"
+    puts "Done scraping, file saved to data/ids/#{File.basename(@json_file)}"
   end
 end
 
 puts 'Input instrument name to scrape ids from:'
-json_file_name = gets.chomp
-scraper = Scraper.new(json_file_name)
+instrument = gets.chomp
+puts 'Input sample type:'
+sample_type = gets.chomp
+
+scraper = Scraper.new(instrument, sample_type)
 scraper.get_json_data
