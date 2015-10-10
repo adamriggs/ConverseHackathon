1483042713f49740715141f05e63f37aab5aa408
Adam Riggs <phlux1111@gmail.com>
2015-10-10 10:39:30 -0400

    TWEAKED THE STRUCTURE
    I abstracted the app object from the setup routines.

---
+++ js/App.js
@@ -0,0 +1,45 @@
+"use strict"
+
+function App(){
+	
+	this.window = {
+		$el: $(window),
+		height: 0,
+		width: 0,
+		gridLeft: 0
+	};
+}
+
+App.prototype.bindWindowResize = function(){
+	var _this = this;
+
+	// Window resize event
+	this.window.$el.on("resize", function(Evt) {
+		_this.setHandleWindowResizeTimeout();
+		Evt.preventDefault();
+		Evt.stopPropagation();
+		return false;
+	});
+	
+	this.window.$el.on("orientationchange", function(Evt) {
+	  _this.setHandleWindowResizeTimeout();
+	});
+}
+
+App.prototype.setHandleWindowResizeTimeout = function(){
+	var _this = this;
+	if(Modernizr.isInputTouchOnly){
+		clearTimeout(this.resizeTimeout);
+		this.resizeTimeout = setTimeout(function(){
+			_this.handleWindowResize(false);
+		}, 500);
+	}
+	else {
+		this.handleWindowResize(false);
+	}
+};
+
+App.prototype.handleWindowResize = function(){
+	this.window.width = window.outerWidth && window.outerWidth > this.window.$el.width() ? window.outerWidth : this.window.$el.width();
+	this.window.height = this.window.$el.height();
+}
--- js/main.js
+++ js/main.js
@@ -1,45 +1,21 @@
 "use strict"
 
-function ConverseApp(){
-	
-	this.window = {
-		$el: $(window),
-		height: 0,
-		width: 0,
-		gridLeft: 0
-	};
-}
+var GLOBAL_VARS = {
 
-ConverseApp.prototype.bindWindowResize = function(){
-	var _this = this;
-
-	// Window resize event
-	this.window.$el.on("resize", function(Evt) {
-		_this.setHandleWindowResizeTimeout();
-		Evt.preventDefault();
-		Evt.stopPropagation();
-		return false;
-	});
-	
-	this.window.$el.on("orientationchange", function(Evt) {
-	  _this.setHandleWindowResizeTimeout();
-	});
-}
-
-ConverseApp.prototype.setHandleWindowResizeTimeout = function(){
-	var _this = this;
-	if(Modernizr.isInputTouchOnly){
-		clearTimeout(this.resizeTimeout);
-		this.resizeTimeout = setTimeout(function(){
-			_this.handleWindowResize(false);
-		}, 500);
-	}
-	else {
-		this.handleWindowResize(false);
-	}
 };
 
-ConverseApp.prototype.handleWindowResize = function(){
-	this.window.width = window.outerWidth && window.outerWidth > this.window.$el.width() ? window.outerWidth : this.window.$el.width();
-	this.window.height = this.window.$el.height();
-}
+var app;
+
+//Document ready
+$( document ).ready(function() {
+    console.log("***** DOM LOAD COMPLETE *****");
+
+    buildApp();
+});
+
+function buildApp() {
+    console.log("buildApp()");
+    app = new App();
+
+    console.log("***** APP BUILD COMPLETE *****");
+};
