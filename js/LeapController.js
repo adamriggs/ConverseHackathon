976f0f27ec50d25bcadf3748e3c4dafa313e3f81
Adam Riggs <phlux1111@gmail.com>
2015-10-10 20:55:42 -0400

    LEAP MOTION WORKS

--- js/LeapController.js
+++ js/LeapController.js
@@ -10,16 +10,17 @@ function LeapController() {
 
 	Leap.loop({}, function(frame){
 		//console.log("leap loop");
-		console.log(frame.id);
+		//console.log(frame.id);
 		//if(frame.id%10==0){
 			for (var i = 0, len = frame.hands.length; i < len; i++) {
 				hand = frame.hands[0];
 
 				palmVect = _this.paramaterizeVector(hand.palmPosition);
 
-				//panner.setPosition(palmVect[0], palmVect[1], palmVect[2]);
+				panner.setPosition(-palmVect[0], palmVect[1], palmVect[2]/-10);
 				//panner.setPosition(palmVect[0], palmVect[1], .5);
 				//console.log(palmVect[0], palmVect[1], palmVect[2]);
+				console.log(palmVect[2]);
 
 				//panner.setOrientation(Math.cos(angle), -Math.sin(angle), 1);
 				//panner.connect(context.destination);
@@ -30,9 +31,14 @@ function LeapController() {
 
 LeapController.prototype.paramaterizeVector = function(Vect) {
 
-	if(Vect[0]<0){Vect[0]=Vect[0]*-1;};
-	if(Vect[1]<1){Vect[1]=Vect[1]*-1;};
-	if(Vect[2]<2){Vect[2]=Vect[2]*-1;};
+	//if(Vect[0]<0){Vect[0]=Vect[0]*-1;};
+	//if(Vect[1]<1){Vect[1]=Vect[1]*-1;};
+	if(Vect[2]<2){
+		Vect[2]=Vect[2]*-1;
+		if(Vect[2]>10){
+			Vect[2] = Vect[2]/10;
+		}
+	};
 
 	return Vect;
 
