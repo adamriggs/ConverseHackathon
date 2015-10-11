ef0dbd1e8e316f093a30e02b47b2b47fde179f24
Adam Riggs <phlux1111@gmail.com>
2015-10-10 20:09:01 -0400

    ADDED BASIC PANNING CAPABILITIES

--- js/App.js
+++ js/App.js
@@ -9,7 +9,36 @@ function App(){
 		gridLeft: 0
 	};
 
+	var WIDTH = this.window.$el.width();
+	var HEIGHT = this.window.$el.height();
+	//var centerX = 640;
+	//var centerY = 360;
+	var X = 500;
+	var Y = 0;
+	var angle = 0;
+
 	GLOBAL_VARS.audioContext = new AudioContext();
+
+	var context = GLOBAL_VARS.audioContext;
+	context.listener.setPosition(0,0,0);
+	var panner = context.createPanner();
+	var centerX = WIDTH/2;
+	var centerY = HEIGHT/2;
+	var x = (X - centerX)  / WIDTH;
+	// The y coordinate is flipped to match the canvas coordinate space.
+	var y = (Y - centerY) / HEIGHT;
+	// Place the z coordinate slightly in behind the listener.
+	var z = -0.5;
+	// Tweak multiplier as necessary.
+	var scaleFactor = 2;
+	panner.setPosition(x * scaleFactor, y * scaleFactor, z);
+	//panner.setPosition(10, 10, 10);
+	// Convert angle into a unit vector.
+	panner.setOrientation(Math.cos(angle), -Math.sin(angle), 1);
+
+	GLOBAL_VARS.panner = panner;
+
+	panner.connect(context.destination);
 
 
 	this.sampleController = new SampleController();
